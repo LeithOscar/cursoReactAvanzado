18995897f6aa9de787445ada44cce317d210bf2f
leith.jarayseh <leith.jarayseh@wolterskluwer.com>
2020-02-16 11:15:10 +0100

    git install standard and config to now

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const HtmlWebpackplugin = require('html-webpack-plugin') 
-module.exports={
-    output:{
-        filename:'app.bundle.js'
-    },
-    plugins:[
-        new HtmlWebpackplugin({
-            template:'src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-                {
-                        test: /\.js$/,
-                        exclude: /node_modules/,
-                        use: {
-                                loader: 'babel-loader',
-                                options: {
-                                        presets: [
-                                                '@babel/preset-env',
-                                                '@babel/preset-react'
-                                        ]
+const HtmlWebpackplugin = require('html-webpack-plugin')
+module.exports = {
+        output: {
+                filename: 'app.bundle.js'
+        },
+        plugins: [
+                new HtmlWebpackplugin({
+                        template: 'src/index.html'
+                })
+        ],
+        module: {
+                rules: [
+                        {
+                                test: /\.js$/,
+                                exclude: /node_modules/,
+                                use: {
+                                        loader: 'babel-loader',
+                                        options: {
+                                                presets: [
+                                                        '@babel/preset-env',
+                                                        '@babel/preset-react'
+                                                ]
+                                        }
                                 }
                         }
-                }
-        ]
-}
+                ]
+        }
 }
