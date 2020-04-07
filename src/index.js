b1375274ef6ebb9308f4afabe0240b686d45c952
Leith Oscar Jarayseh Gutierrez <leith.jarayseh@wolterskluwer.com>
2020-04-07 12:25:16 +0200

    apollo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './app'
+import { App } from './app'
+import ApolloClient from 'apollo-boost'
+import ApolloProvider from 'react-apollo'
 
-ReactDOM.render(<App />,
-document.getElementById('app'));
+
+const client = new ApolloClient({
+    uri: 'https://petgram-server-jcamacaro.camacaro.now.sh/graphql'
+})
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
