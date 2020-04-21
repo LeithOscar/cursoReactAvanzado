6f5a09391e8d33da583849d4dea195cb19867c84
Leith Oscar Jarayseh Gutierrez <leith.jarayseh@wolterskluwer.com>
2020-04-21 13:55:55 +0200

    fix

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import {Anchor, Image} from './styles.js'
-const DEFAULT_IMAGE="https://i.imgur.com/dJa0Hpl.jpg";
+import React from './node_modules/react'
+import { Anchor, Image } from './styles.js'
+const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpg";
 
-export const Category=({cover=DEFAULT_IMAGE, path,emoji='?'}) =>(
+export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
 
     <Anchor href={path}>
-    <Image src={cover} />
-    {emoji}
+        <Image src={cover} />
+        {emoji}
     </Anchor>
 )
