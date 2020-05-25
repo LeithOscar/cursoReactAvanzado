01cd35172ff1814dad656c563226752e2eb376bb
Leith Oscar Jarayseh Gutierrez <leith.jarayseh@wolterskluwer.com>
2020-05-25 12:33:05 +0200

    ReactContext

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 
+import { Favs } from './Pages/Favs'
+import { User } from './Pages/User'
+//import { NotRegisteredUser } from './Pages/NotregisteredUser'
+
 import { Router } from '@reach/router'
+import { NotRegisteredUser } from './Pages/NotregisteredUser'
+
+
+const UserLogged = ({ children }) => {
+    return children({ isAuth: false })
+}
 
 export const App = () => {
     return (
@@ -17,8 +27,24 @@ export const App = () => {
                 <Home path='/' />
                 <Home path='/pet/:categoryId' />
                 <Detail path='/detail/:detailId' />
-
             </Router>
+            <UserLogged>
+
+                {
+                    ({ isAuth }) => isAuth
+
+                        ? <Router>
+                            <Favs path='/favs' />
+                            <User path='/user' />
+                        </Router>
+                        : <Router>
+                            <NotRegisteredUser path='/favs' />
+                            <NotRegisteredUser path='/user' />
+                        </Router>
+                }
+
+            </UserLogged>
+
             <NavBar />
         </div>
     )
---
+++ src/Pages/NotregisteredUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotRegisteredUser = () => (
+    <h1>NotRegistereduser</h1>
+)
